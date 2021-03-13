2b962f7ff96e92d98eed6dc11122b22d89e96308
Doubtingeye352 <69747012+Doubtingeye352@users.noreply.github.com>
2021-03-13 23:38:45 +0530

    Finish homepage gui

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.button import MDRectangleFlatButton
 
 screen_helper = """
 ScreenManager:
     MenuScreen:
-    Planetselector:
+    Planets:
     Earth:
-    
-    
 <MenuScreen>:
     name: 'menu'
-   
-
-<Planetselector>:
-    name: 'Planetselector'
+    
+    
+    Carousel:
+        direction: 'right'
+        pos_hint:{"center_x":0.5,"center_y":0.7}  
+        AsyncImage:
+            source: 'http://placehold.it/480x270.png&text=slide-1.png'
+        AsyncImage:
+            source: 'http://placehold.it/480x270.png&text=slide-2.png'
+        AsyncImage:
+            source: 'http://placehold.it/480x270.png&text=slide-3.png'
+        AsyncImage:
+            source: 'http://placehold.it/480x270.png&text=slide-4.png'
+            
+    MDLabel:
+        text: "Welcome to Planets, Choose your option: "
+        pos_hint:{"center_x":0.8,"center_y":0.3}
+        
+    MDRectangleFlatButton:
+        text: "Planet selection"
+        pos_hint:{"center_x":0.5,"center_y":0.2}
+        
+    MDRectangleFlatButton:
+        text: "About us"
+        pos_hint:{"center_x":0.5,"center_y":0.1}    
     
 
+<Planets>:
+    name: 'Planets'
+   
 <Earth>:
     name: 'Earth'
-   
+
 
 """
 
@@ -28,7 +51,7 @@ class MenuScreen(Screen):
     pass
 
 
-class Planetselector(Screen):
+class Planets(Screen):
     pass
 
 
@@ -39,16 +62,15 @@ class Earth(Screen):
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
-sm.add_widget(Planetselector(name='Planetselector'))
+sm.add_widget(Planets(name='Planets'))
 sm.add_widget(Earth(name='Earth'))
 
 
-class Alpha_build(MDApp):
+class DemoApp(MDApp):
 
     def build(self):
         screen = Builder.load_string(screen_helper)
-        self.title = "Planets Dev version"
         return screen
 
 
-Alpha_build().run()
+DemoApp().run()
